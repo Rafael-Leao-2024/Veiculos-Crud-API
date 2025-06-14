4e34105c5d3b3fcc925105c85a5394b578f7ce30
RafaelLeao <146947546+Rafael-Leao-2024@users.noreply.github.com>
2025-06-14 14:15:58 -0300

    json schema extra

--- app/rotas/user.py
+++ app/rotas/user.py
@@ -2,10 +2,9 @@ from fastapi import APIRouter, status, Depends, HTTPException
 from app.schema.schema_usuario import Usuario, UsuarioInDB, UsuarioOutput
 from app.manage_database.sessao_db import get_db
 from werkzeug.security import generate_password_hash
-from app.rotas import login
+from app.rotas.login import pegar_usuario_atual_ativo
 
-
-route_user = APIRouter(prefix="/usuarios", tags=['Usuarios'], dependencies=[Depends(login.pegar_usuario_atual_ativo)])
+route_user = APIRouter(prefix="/usuarios", tags=['Usuarios'], dependencies=[Depends(pegar_usuario_atual_ativo)])
 
 @route_user.get('/', status_code=status.HTTP_200_OK, response_model=list[UsuarioOutput])
 async def listar_usuarios(cursor=Depends(get_db)):
@@ -28,7 +27,7 @@ async def buscar_usuario(id_usuario: int, cursor=Depends(get_db)):
 
 @route_user.post('/create-user', status_code=status.HTTP_201_CREATED,response_model=UsuarioOutput)
 async def criar_usuario(usuario: Usuario, cursor=Depends(get_db)):
-    if not usuario.nome and not usuario.email and not usuario.senha:
+    if not usuario.nome or not usuario.email or not usuario.senha:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Os Campos nao foram preenchidos")
     dicionario = usuario.model_dump(exclude_unset=True)
     dicionario.update({"id": 1})
@@ -36,6 +35,7 @@ async def criar_usuario(usuario: Usuario, cursor=Depends(get_db)):
     comando_sql = '''INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?); '''
     cursor.execute(comando_sql,  (usuario.nome, usuario.email, generate_password_hash(usuario.senha)))
     usuario.id = cursor.lastrowid
+    print(usuario)
     return usuario
 
 
--- app/schema/schema_usuario.py
+++ app/schema/schema_usuario.py
@@ -8,6 +8,19 @@ class Usuario(BaseModel):
     disabled: Optional[bool] = False
     senha: str
 
+    class Config:
+        json_schema_extra = {
+            "examples":[
+                {
+                    "nome": "Seu nome Completo",
+                    "email": "email valido",
+                    "disabled": False,
+                    "senha": "Senha"
+                }
+            ]
+        }
+
+
 class UsuarioInDB(Usuario):
     id: int
         
